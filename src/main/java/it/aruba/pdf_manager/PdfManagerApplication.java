85ed971be2233d57150588b72e733f4e43e0ce4d
LukaF88 <fissorel@gmail.com>
2020-12-08 14:41:00 +0100

    setup swagger

--- src/main/java/it/aruba/pdf_manager/PdfManagerApplication.java
+++ src/main/java/it/aruba/pdf_manager/PdfManagerApplication.java
@@ -5,9 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.models.Contact;
 import it.aruba.pdf_manager.services.DocumentService;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PdfManagerApplication extends SpringBootServletInitializer {
 
 	@Autowired
@@ -20,4 +31,16 @@ public class PdfManagerApplication extends SpringBootServletInitializer {
 		SpringApplication.run(PdfManagerApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("it.aruba.pdf_manager")).paths(PathSelectors.regex("/.*"))
+				.build().apiInfo(apiEndPointsInfo());
+	}
+
+	private ApiInfo apiEndPointsInfo() {
+		return new ApiInfoBuilder().title("Spring Boot REST API").description("Employee Management REST API")
+				.contact("Luica Fissore").license("Apache 2.0")
+				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html").version("1.0.0").build();
+	}
 }
